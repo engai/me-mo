0ba556eec726ac8da6099968ec68e01b327fb135
Ed Ngai <engai@ucsd.edu>
2016-11-26 01:06:48 -0800

    Moving analytics code to component

--- app/components/add.js
+++ app/components/add.js
@@ -22,8 +22,22 @@ class Add extends Component {
   componentWillMount() {
     var script = document.createElement( 'script' );
     script.type = 'text/javascript';
-    script.src = "../ga.js";
     script.async = true;
+
+    let hi = `function utmx_section(){}function utmx(){}(function(){var
+    k='134638487-0',d=document,l=d.location,c=d.cookie;
+    if(l.search.indexOf('utm_expid='+k)>0)return;
+    function f(n){if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.
+    indexOf(';',i);return escape(c.substring(i+n.length+1,j<0?c.
+    length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;d.write(
+    '<sc'+'ript src="'+'http'+(l.protocol=='https:'?'s://ssl':
+    '://www')+'.google-analytics.com/ga_exp.js?'+'utmxkey='+k+
+    '&utmx='+(x?x:'')+'&utmxx='+(xx?xx:'')+'&utmxtime='+new Date().
+    valueOf()+(h?'&utmxhash='+escape(h.substr(1)):'')+
+    '" type="text/javascript" charset="utf-8"><\/sc'+'ript>')})();`
+
+    script.append(hi);
+    script.append("utmx('url','A/B');")
     document.head.prepend(script);
   }
 
